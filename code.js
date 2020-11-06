e0bd201d103d7a3cac17952a5265674e38cf9273
dd395 <drichardssmith@my.wctc.edu>
2020-11-06 10:40:39 -0600

    Changed input box to inside the table

--- code.js
+++ code.js
@@ -1,30 +1,29 @@
 $(document).ready(function(){
     $("form").submit(placeOrder);
 
+
+
     function placeOrder(event) {
         //prevent form from flashing and resetting
         event.preventDefault();
 
         console.log("entering placeOrder");
 
-        var prodLJ = parseFloat(document.getElementById('#prodLJ').value);
+       var prodLJ = parseFloat($("#prodLJ").val());
 
+        console.log(prodLJ);
 
-       console.log(prodLJ);
-
-        var prodDon = parseFloat(document.getElementById('#prodDon').value);
+        var prodDon = parseFloat($("#prodDon").val());
 
         console.log(prodDon);
 
         var prodLJTotal = prodLJ * 39.99;
         var prodDonTotal = prodDon * 14.99;
         var grandTotal = prodLJTotal + prodDonTotal;
-        var qt1 = prodLJ;
-        var qt2 = prodDon;
+
         console.log("exiting placeOrder");
 
-        $("#qt1").text(qt1.toFixed(0));
-        $("#qt2").text(qt2.toFixed(0));
+
         $("#prodLJTotal").text(prodLJTotal.toLocaleString('en-US', { style: 'currency', currency: 'USD' }));
         $("#prodDonTotal").text(prodDonTotal.toLocaleString('en-US', { style: 'currency', currency: 'USD' }));
         $("#grandTotal").text(grandTotal.toLocaleString('en-US', { style: 'currency', currency: 'USD' }));
